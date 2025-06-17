7cecd9d0550884e8087fd2c00b65536093f69de2
Caleb <calebsebsibe.custor@gmail.com>
2025-06-17 21:40:06 +0300

    fixed profile render and required

--- src/app/core/Validator/englishValidator.ts
+++ src/app/core/Validator/englishValidator.ts
@@ -1,20 +1,51 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+// import { Directive, ElementRef, HostListener } from '@angular/core';
+
+// @Directive({
+//   selector: '[englishOnly]',
+//   standalone: true
+// })
+// export class EnglishOnlyDirective {
+//   private regex: RegExp = /^[\u0041-\u005A\u0061-\u007A\s]*$/;
+  
+
+//   constructor(private el: ElementRef) {}
+
+//   @HostListener('input', ['$event']) onInput(event: any) {
+//     const input = this.el.nativeElement.value;
+//     if (!this.regex.test(input)) {
+//       this.el.nativeElement.value = input.replace(/[^\u0041-\u005A\u0061-\u007A\s]/g, '');
+//       event.stopPropagation();
+//     }
+//   }
+// }
+import { Directive, ElementRef, HostListener, inject } from '@angular/core';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[englishOnly]',
   standalone: true
 })
 export class EnglishOnlyDirective {
-  private regex: RegExp = /^[\u0041-\u005A\u0061-\u007A\s]*$/;
-  
+  private el = inject(ElementRef<HTMLInputElement>);
+  private control = inject(NgControl);
 
-  constructor(private el: ElementRef) {}
+  @HostListener('input', ['$event'])
+  onInput(event: Event): void {
+    const inputEl = this.el.nativeElement;
+    const originalValue = inputEl.value;
 
-  @HostListener('input', ['$event']) onInput(event: any) {
-    const input = this.el.nativeElement.value;
-    if (!this.regex.test(input)) {
-      this.el.nativeElement.value = input.replace(/[^\u0041-\u005A\u0061-\u007A\s]/g, '');
-      event.stopPropagation();
+    const sanitized = originalValue.replace(/[^A-Za-z\s]/g, '');
+
+    if (originalValue !== sanitized) {
+      inputEl.value = sanitized;
+    }
+
+    if (this.control.control) {
+      this.control.control.setValue(sanitized, {
+        emitEvent: false,
+      });
+      this.control.control.markAsDirty();
+      this.control.control.updateValueAndValidity();
     }
   }
 }
